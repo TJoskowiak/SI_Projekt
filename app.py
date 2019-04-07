ed8eaf3e786c1a328b6e0910505416c131bca2e7
Pietrucci <filippiet@gmail.com>
2019-04-08 00:41:35 +0200

    GraphDrawer SATSolver init

--- app.py
+++ app.py
@@ -1,2 +1,22 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+
 if __name__ == "__main__":
-    pass
+    G = nx.Graph()
+
+
+    G.add_edges_from([(1, 2),
+                      (2, 3),
+                      (3, 4),
+                      (4, 1),
+                      (4, 5),
+                      (5, 6),
+                      (6, 2)])
+    print(G.nodes())
+    print(G.edges())
+
+
+    colors = [1,2,1,1,1,1]
+
+    nx.draw(G, node_color=colors, node_size=800)
+    plt.show()
